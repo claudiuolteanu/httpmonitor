6d3f0e85b84dbacc154045603775b848bf0230fe
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2019-12-01 10:07:49 +0200

    Create subpackage with monitoring files.
    Signed-off-by: Claudiu Olteanu <olteanu.claudiu@ymail.com>

---
+++ monitor/monitor.go
@@ -0,0 +1,93 @@
+package monitor
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/hpcloud/tail"
+	"github.com/prometheus/tsdb/labels"
+)
+
+type Monitor struct {
+	db         *LoggingDatabase
+	wg         *sync.WaitGroup
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+	filename   string
+}
+
+func NewMonitor(filename string) (*Monitor, error) {
+	db, err := NewLoggingDatabase()
+	if err != nil {
+		return nil, err
+	}
+
+	wg := &sync.WaitGroup{}
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	return &Monitor{db: db, wg: wg, filename: filename, ctx: ctx, cancelFunc: cancelFunc}, nil
+}
+
+func (m *Monitor) processLogs() {
+	defer m.wg.Done()
+
+	t, err := tail.TailFile(m.filename, tail.Config{Follow: true})
+	if err != nil {
+		// TODO return error (use errors channel)
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case line := <-t.Lines:
+			entry, err := NewLoggingEntry(line.Text)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				err = m.db.AddEntry(entry)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		case <-m.ctx.Done():
+			fmt.Println("Stop processing logs")
+			return
+		}
+	}
+}
+
+func (m *Monitor) monitorLogs() {
+	defer m.wg.Done()
+
+	query := labels.NewEqualMatcher(RequestMethodLabel, "GET")
+
+	for {
+		select {
+		case <-time.After(10 * time.Second):
+			err := m.db.Query(1571047299, 1576061842, query)
+
+			if err != nil {
+				fmt.Printf("Failed to sync data: %v", err)
+			}
+		case <-m.ctx.Done():
+			fmt.Println("Stop monitoring logs")
+			return
+		}
+	}
+}
+
+func (m *Monitor) Start() {
+	m.wg.Add(2)
+	go m.processLogs()
+	go m.monitorLogs()
+}
+
+func (m *Monitor) Stop() error {
+	m.cancelFunc()
+	m.wg.Wait()
+
+	return m.db.Cleanup()
+}
